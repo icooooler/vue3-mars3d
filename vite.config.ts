cfe5d285a038df5508da14ac531ddaefc99774f8
zhuxiaobo <zhxb@olinkstar.com>
2024-09-06 10:53:07 +0800

    set host

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import { vitePluginMars3d } from 'vite-plugin-mars3d'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+      host: '0.0.0.0'
+    },
     plugins: [
         vue(),
         dts({ include: './packages' }),
